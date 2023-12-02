5aaf6626506afc397600494cb46e8677aa04a1eb
TMrex-dev <123335008+TMrex-dev@users.noreply.github.com>
2023-12-02 00:29:24 -0500

    YO THE SEARCH FILTER IS FUNCTIONAL

--- src/app/search-bar/search-bar.component.ts
+++ src/app/search-bar/search-bar.component.ts
@@ -1,29 +1,24 @@
 import { Component } from '@angular/core';
 
-
 @Component({
   selector: 'app-search-bar',
   templateUrl: './search-bar.component.html',
-  styleUrl: './search-bar.component.css',
-
+  styleUrls: ['./search-bar.component.css'], // Update styleUrls property
 })
 export class SearchBarComponent {
-  items: any[] = []; // Your list of items
+  items: any[] = [
+    { name: 'cs2' },
+    { name: 'club penguin' },
+    { name: 'pico park' }
+    // Other items
+  ];
   filteredItems: any[] = [];
   searchTerm: string = '';
   submittedSearchTerm: string = '';
 
   constructor() {
-    // Fetch or initialize your items array here
-    this.items = [
-      { name: 'cs2' },
-      { name: 'club penguin' },
-      { name: 'pico park'}
-      // Other items
-    ];
     this.filteredItems = this.items; // Initially set filteredItems to all items
   }
-
 
   filterItems() {
     this.filteredItems = this.items.filter(item =>
@@ -32,14 +27,13 @@ export class SearchBarComponent {
   }
 
   onItemClick(item: any) {
-    // Implement what happens when an item from suggestions is clicked (e.g., select the item, perform an action, etc.)
     console.log('Selected Item:', item);
-    this.searchTerm = item.name; // Set search term when an item is clicked (optional)
-    this.filteredItems = []; // Clear the suggestions after selection (optional)
+    this.searchTerm = item.name;
+    this.filteredItems = [];
   }
 
   submitSearch() {
-    this.submittedSearchTerm = this.searchTerm
-    console.log('Search:', this.submittedSearchTerm)
+    this.submittedSearchTerm = this.searchTerm;
+    console.log('Search:', this.submittedSearchTerm);
   }
 }
