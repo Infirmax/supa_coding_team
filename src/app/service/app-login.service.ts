858a95760c1aefdf565149a2914caa374a4c7d5c
kalin1233 <kalin32@bu.edu>
2023-12-03 22:19:01 -0500

    Initial commit for third party OAuth
    Starting point

---
+++ src/app/service/app-login.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { GoogleLoginProvider, SocialAuthService, SocialUser } from 'angularx-social-login';
+import { from } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AppLoginService {
+  public isLoggedIn: boolean;
+  public user: SocialUser | undefined;
+
+  constructor(private authService: SocialAuthService, private router: Router) {
+    this.isLoggedIn = false;
+  }
+
+  googleLogIn() {
+    return from(this.authService.signIn(GoogleLoginProvider.PROVIDER_ID));
+  }
+
+  logIn() {
+    this.googleLogIn().subscribe((user: SocialUser) => {
+      this.user = user;
+      this.isLoggedIn = true;
+      console.log(`LogIn this.user: ${JSON.stringify(this.user)}`);
+    });
+  }
+
+  logOut() {
+    this.authService.signOut().then(() => {
+      console.log(`LogOut this.user: ${JSON.stringify(this.user)}`);
+      this.user = undefined;
+      this.isLoggedIn = false;
+      this.router.navigate(['./home']);
+    });
+  }
+}
+
+
+
